6d9e482f63c34517bad0ded21aefbe9299799bbd
Jeongbowoon <happyanne200212@gmail/com>
2024-05-05 00:28:23 +0900

    feat: main page layout

---
+++ app/post/share/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function PostSharedPage() {
+
+
+    return (<main >
+        <section>
+            <h1>GitHub ID 의 글</h1>
+            //myPosts 가져오기
+        </section>
+    </main>
+    );
+}
---
+++ component/MyPost.tsx
@@ -0,0 +1,21 @@
+//my post - DB에서 가져오기
+
+//임시 데이터
+export const myPosts = [
+    {
+        title: 'mypost1',
+        content:'this is my post1',
+    },
+    {
+        title: 'mypost2',
+        content:'this is my post2',
+    },
+    {
+        title: 'mypost3',
+        content:'this is my post3',
+    },
+    {
+        title: 'mypost4',
+        content:'this is my post4',
+    },
+]
