fc38fc9e14a7f6309f3b3db6ab936573168bd1f6
Lee Euije <exitfill1234@naver.com>
2024-05-28 22:44:03 +0900

    feat: vercel function maxDuration 설정
    https://vercel.com/guides/what-can-i-do-about-vercel-serverless-functions-timing-out

--- api/openai.ts
+++ api/openai.ts
@@ -4,6 +4,10 @@ import { openai } from '@ai-sdk/openai';
 import { streamText } from 'ai';
 import { createStreamableValue } from 'ai/rsc';
 
+export const config = {
+  maxDuration: 60,
+};
+
 export async function generateCompletion(prompt: string) {
   'use server';
 
